7cf5de3585722c6e191f1e84ca823ca2b3c9748b
Vance <vance@axxe.co.uk>
2015-10-08 18:47:16 +0100

    Updated setup.py based on my fork

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='keystone',
-      version='0.1',
+      version='0.11',
       description='Python Library for the Monkeyboard DAB/DAB+ FM Digital Radio Board',
-      author='Myles Eftos',
-      author_email='myles@madpilot.com.au',
-      url='https://github.com/madpilot/keystonepy/',
+      author='Vance Briggs, based on previous work by Myles Eftos',
+      author_email='vance@axxe.co.uk',
+      url='https://github.com/vanceb/keystonepy/',
       packages=['keystone'],
      )
